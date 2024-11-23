8efe2eef262dc56529e7aa645a4a30e46b74063b
Chance03-git <Chancefoster05@gmail.com>
2024-11-22 21:56:58 -0500

    documentation for next ten numbers function

--- suppplement4.py
+++ suppplement4.py
@@ -1,4 +1,12 @@
 def next_ten_numbers(num):
+    """Generates the next 10 numbers from the given number as a comma-delimited string.
+
+    Args:
+        num: The starting number.
+
+    Returns:
+        A string containing the next 10 numbers separated by commas.
+    """
     return ','.join(str(num + i) for i in range (1, 11))
 def test_should_return_next_ten_numbers_as_comma_delimited_string():
     assert next_ten_numbers(5) == "6,7,8,9,10,11,12,13,14,15"
