f405b434eeef254e1c29d06e15edd48f3128b1dd
Chance03-git <Chancefoster05@gmail.com>
2024-11-22 22:14:26 -0500

    documentation for Function for list to comma delimited string

--- suppplement4.py
+++ suppplement4.py
@@ -10,6 +10,14 @@ def next_ten_numbers(num):
     return ','.join(str(num + i) for i in range (1, 11))
 
 def list_to_comma_delimited_string(strings):
+    """Converts a list of strings to a comma-delimited string.
+
+    Args:
+        strings: A list of strings.
+
+    Returns:
+        A single string where each element is separated by a comma.
+    """
     return ','.join(strings)
 def test_should_return_next_ten_numbers_as_comma_delimited_string():
     assert next_ten_numbers(5) == "6,7,8,9,10,11,12,13,14,15"
