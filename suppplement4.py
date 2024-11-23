b590644ca15a90007463de2afb47b9f50a0f0c12
Chance03-git <Chancefoster05@gmail.com>
2024-11-22 21:55:43 -0500

    function for next 10 numbers

--- suppplement4.py
+++ suppplement4.py
@@ -1,2 +1,4 @@
+def next_ten_numbers(num):
+    return ','.join(str(num + i) for i in range (1, 11))
 def test_should_return_next_ten_numbers_as_comma_delimited_string():
     assert next_ten_numbers(5) == "6,7,8,9,10,11,12,13,14,15"
