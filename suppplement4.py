04184408902f661a0ccae77a86004c7624a802c9
Chance03-git <Chancefoster05@gmail.com>
2024-11-22 22:08:00 -0500

    Test function for string from length

--- suppplement4.py
+++ suppplement4.py
@@ -10,3 +10,5 @@ def next_ten_numbers(num):
     return ','.join(str(num + i) for i in range (1, 11))
 def test_should_return_next_ten_numbers_as_comma_delimited_string():
     assert next_ten_numbers(5) == "6,7,8,9,10,11,12,13,14,15"
+def test_should_return_comma_delimited_string_from_list():
+    assert list_to_comma_delimited_string(["apple", "banana", "cherry"]) == "apple,banana,cherry"
